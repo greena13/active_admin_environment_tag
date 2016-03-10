7aa2f30f1a7d4f4bf0e528f14d5ce4348fca51db
Aleck Greenham <greenhama13@gmail.com>
2016-03-10 08:01:53 +0000

    Initial commit

---
+++ lib/active_admin_environment_tag.rb
@@ -0,0 +1,47 @@
+require "active_admin_environment_tag/version"
+
+module ActiveAdminEnvironmentTag
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+
+  class << self
+    attr_reader :environment_colors
+
+    def configuration
+      if block_given?
+        yield(ActiveAdminEnvironmentTag)
+      end
+    end
+
+    def environment_colors= (new_colours)
+      @environment_colors.merge!(new_colours)
+    end
+
+    alias :config :configuration
+    alias :configure :configuration
+  end
+
+  @environment_colors = {
+      development: '#91CA4B',
+      production: '#fc1f27',
+      default: '#c9d6e3'
+  }
+end
+
+
+module ActiveAdmin
+  module Views
+    class Footer < Component
+      def build
+        super id: "footer"
+        super style: "text-align: right;"
+
+        div class: 'active-admin-environment-tag' do
+          div Rails.env
+        end
+      end
+    end
+  end
+end
---
+++ lib/active_admin_environment_tag/version.rb
@@ -0,0 +1,3 @@
+module ActiveAdminEnvironmentTag
+  VERSION = "0.0.1"
+end
